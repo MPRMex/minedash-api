c88fa2484cc8982dc884d5777ba797d339a1a453
Roberto Carlos García Luis <roberto.garcia@mprmex.com>
2022-11-04 13:07:03 -0600

    Adding a type for corpId

--- models.go
+++ models.go
@@ -1,22 +1,38 @@
 package minedash
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
 type EntityAllResponse struct {
 	Entities []Entity `json:"entity"`
 }
 
 type Entity struct {
-	CorpId      Str    `json:"corpId"`
-	EntityLabel string `json:"entityLabel"`
-	EntityType  string `json:"entityType"`
-	ExternalId  string `json:"externalId"`
-	OnSite      bool   `json:"onSite"`
-	SysId       int    `json:"sysId"`
+	CorpId      Str         `json:"corpId"`
+	EntityLabel string      `json:"entityLabel"`
+	EntityType  string      `json:"entityType"`
+	ExternalId  string      `json:"externalId"`
+	OnSite      bool        `json:"onSite"`
+	SysId       json.Number `json:"sysId"`
 }
 
 type Str string
 
 func (w *Str) UnmarshalJSON(data []byte) (err error) {
-	v := Str(string(data))
-	w = &v
+	var s string
+	var i int
+	result := ""
+	if err := json.Unmarshal(data, &s); err == nil {
+		result = s
+	} else if err := json.Unmarshal(data, &i); err == nil {
+		result = strconv.Itoa(i)
+	}
+	*w = Str(result)
 	return nil
+}
+
+func (w Str) String() string {
+	return string(w)
 }
