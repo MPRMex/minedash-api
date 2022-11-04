12bf08c1849528258c5c42faa4ef92da8799a6cc
Roberto Carlos García Luis <roberto.garcia@mprmex.com>
2022-11-03 20:28:36 -0600

    Fixing a type

--- models.go
+++ models.go
@@ -1,14 +1,16 @@
 package minedash
 
+import "encoding/json"
+
 type EntityAllResponse struct {
 	Entities []Entity `json:"entity"`
 }
 
 type Entity struct {
-	CorpId      string `json:"corpId"`
-	EntityLabel string `json:"entityLabel"`
-	EntityType  string `json:"entityType"`
-	ExternalId  string `json:"externalId"`
-	OnSite      bool   `json:"onSite"`
-	SysId       int    `json:"sysId"`
+	CorpId      json.Number `json:"corpId"`
+	EntityLabel string      `json:"entityLabel"`
+	EntityType  string      `json:"entityType"`
+	ExternalId  string      `json:"externalId"`
+	OnSite      bool        `json:"onSite"`
+	SysId       int         `json:"sysId"`
 }
