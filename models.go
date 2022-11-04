9f953f9bf88895428e69c458680eb8a76fe5ed4e
Roberto Carlos García Luis <roberto.garcia@mprmex.com>
2022-11-04 11:43:29 -0600

    Trying with byte type for corpId

--- models.go
+++ models.go
@@ -1,16 +1,22 @@
 package minedash
 
-import "encoding/json"
-
 type EntityAllResponse struct {
 	Entities []Entity `json:"entity"`
 }
 
 type Entity struct {
-	CorpId      json.Number `json:"corpId"`
-	EntityLabel string      `json:"entityLabel"`
-	EntityType  string      `json:"entityType"`
-	ExternalId  string      `json:"externalId"`
-	OnSite      bool        `json:"onSite"`
-	SysId       int         `json:"sysId"`
+	CorpId      Str    `json:"corpId"`
+	EntityLabel string `json:"entityLabel"`
+	EntityType  string `json:"entityType"`
+	ExternalId  string `json:"externalId"`
+	OnSite      bool   `json:"onSite"`
+	SysId       int    `json:"sysId"`
+}
+
+type Str string
+
+func (w *Str) UnmarshalJSON(data []byte) (err error) {
+	v := Str(string(data))
+	w = &v
+	return nil
 }
